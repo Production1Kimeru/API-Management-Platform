da675d06c462669c949e2ce372aa37393b5837fc
NoahGillard <noah.gillard@telenet.be>
2020-09-07 14:22:09 +0200

    breadcrumbs overview page

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -15,8 +15,8 @@
                 <!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Starter Page</li>
+                        {{--<li class="breadcrumb-item"><a href="#">Home</a></li><--}}
+                        <li class="breadcrumb-item active">Overview</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
