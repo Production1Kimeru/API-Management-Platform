6b9f2eaed9ebe1c3b0ca8b4b3e2dcaaf8bdd269d
NoahGillard <noah.gillard@telenet.be>
2020-09-08 12:38:25 +0200

    Edit modal + change label add modal

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -9,7 +9,7 @@
                     <h1 class="m-0 text-dark">API Overview</h1>
                     <div class="btn-group" role="group" aria-label="Basic example">
                         <button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#modal-add">Add</button>
-                        <button type="button" class="btn btn-warning btn-sm ml-2">Edit</button>
+                        <button type="button" class="btn btn-warning btn-sm ml-2" data-toggle="modal" data-target="#modal-edit">Edit</button>
                         <button type="button" class="btn btn-danger btn-sm ml-2">Remove</button>
                     </div>
                 </div>
@@ -66,9 +66,8 @@
                         </div>
 
                         <!-- textarea -->
-                        <label>Description API</label>
                         <div class="form-group">
-                            <label>Textarea</label>
+                            <label>Description API</label>
                             <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
                         </div>
                     </div>
@@ -82,7 +81,62 @@
             <!-- /.modal-dialog -->
         </div>
         <!-- /.Modal Add -->
+        <!-- Modal Edit -->
+        <div class="modal fade" id="modal-edit">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Edit API</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label>Name API</label>
+                            <select class="form-control select2 select2-danger" data-dropdown-css-class="select2-danger" style="width: 100%;">
+                                <option selected="selected">Accu Weather</option>
+                                <option>Rails</option>
+                                <option>De Tijd</option>
+                                <option>Random Jokes</option>
+                                <option>Appointment Dentist</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Category</label>
+                            <select class="form-control select2 select2-danger" data-dropdown-css-class="select2-danger" style="width: 100%;">
+                                <option selected="selected">Universal</option>
+                                <option>News</option>
+                                <option>Random</option>
+                                <option>Company specified</option>
+                            </select>
+                        </div>
+                        <!-- /.form-group -->
+                        <label>API Key</label>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
 
+                                <span class="input-group-text"><i class="fas fa-key"></i></span>
+                            </div>
+                            <input type="text" class="form-control" placeholder="API Key">
+                        </div>
+
+                        <!-- textarea -->
+                        <div class="form-group">
+                            <label>Description API</label>
+                            <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                        </div>
+                    </div>
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-warning">Save Changes</button>
+                    </div>
+                </div>
+                <!-- /.modal-content -->
+            </div>
+            <!-- /.modal-dialog -->
+        </div>
+        <!-- /.Modal Edit -->
 
         <div class="container-fluid">
             <div class="row">
